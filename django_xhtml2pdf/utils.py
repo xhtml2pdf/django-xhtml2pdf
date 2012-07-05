985537952b32fa503b895079f1fc4d5397705740
C. R. Oldham <cro@ncbt.org>
2012-07-04 23:39:56 -0600

    Make the fetch_resources link_callback work with the staticfiles module (e.g. loop through the STATICFILES_DIRS and see if a desired resource exists anywhere else).

--- django_xhtml2pdf/utils.py
+++ django_xhtml2pdf/utils.py
@@ -26,6 +26,11 @@ def fetch_resources(uri, rel):
     elif uri.startswith(settings.STATIC_URL):
         path = os.path.join(settings.STATIC_ROOT,
                             uri.replace(settings.STATIC_URL, ""))
+        if not os.path.exists(path):
+            for d in settings.STATICFILES_DIRS:
+                path = os.path.join(d, uri.replace(settings.STATIC_URL, ""))
+                if os.path.exists(path):
+                    break
     else:
         raise UnsupportedMediaPathException(
                                 'media urls must start with %s or %s' % (
