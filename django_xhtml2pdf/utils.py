b59ebc464130c115cf9ef829fec0cfdc529e00c9
Guilherme Vierno <guilhermevierno@gmail.com>
2014-05-30 15:24:12 -0300

    Adds a PdfResponse to the utils.
    PdfResponse is a subclass of django's TemplateResponse which outputs
    pdf.

--- django_xhtml2pdf/utils.py
+++ django_xhtml2pdf/utils.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.template.context import Context
 from django.template.loader import get_template
+from django.template.response import TemplateResponse
 from xhtml2pdf import pisa # TODO: Change this when the lib changes.
 import StringIO
 import os
@@ -75,3 +76,15 @@ def render_to_pdf_response(template_name, context=None, pdfname=None, link_callb
         pdfname = '%s.pdf' % os.path.splitext(os.path.basename(template_name))[0]
     file_object['Content-Disposition'] = 'attachment; filename=%s' % pdfname
     return generate_pdf(template_name, file_object, context, link_callback=link_callback)
+
+
+class PdfResponse(TemplateResponse):
+    def render(self):
+        retval = super(PdfResponse, self).render()
+        result = StringIO.StringIO()
+        pisa.CreatePDF(
+            self.rendered_content,
+            dest=result,
+            link_callback=fetch_resources)
+        self.content = result.getvalue()
+        return retval
